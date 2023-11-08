950302b213134b9a66c966edc7a86e2fd50eced4
burakekinci7 <ekincib015@gmail.com>
2023-11-08 14:54:38 +0300

    Initial Commit

---
+++ CoreDateBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDateBase
+//
+//  Created by Ramazan Burak Ekinci on 8.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
